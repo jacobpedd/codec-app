7fc239614ae2618f18e8b330e735553fced62469
Jacob Peddicord <jpmail17@gmail.com>
2024-05-20 15:50:06 -0700

    capture some touches

--- Codec/Views/TopicListView.swift
+++ Codec/Views/TopicListView.swift
@@ -5,7 +5,6 @@
 //  Created by Jacob Peddicord on 5/17/24.
 //
 
-import Foundation
 import SwiftUI
 
 struct TopicListView: View {
@@ -15,83 +14,86 @@ struct TopicListView: View {
     @State private var isMenuOpen: Bool = false
 
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                ZStack {
-                    if let image = userModel.topicArtworks[topic.id] {
-                        Image(uiImage: image.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 60, height: 60)
-                            .cornerRadius(10)
-                            .overlay(isPlayingTopic ?
-                                     Color.black.opacity(0.6).cornerRadius(10) : nil
-                            )
-                    } else {
-                        Rectangle()
-                            .fill(Color.gray)
-                            .frame(width: 60, height: 60)
-                            .cornerRadius(10)
-                    }
+        ZStack {
+            VStack(spacing: 0) {
+                HStack {
+                    ZStack {
+                        if let image = userModel.topicArtworks[topic.id] {
+                            Image(uiImage: image.image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(10)
+                                .overlay(isPlayingTopic ?
+                                         Color.black.opacity(0.6).cornerRadius(10) : nil
+                                )
+                        } else {
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(10)
+                        }
 
-                    if isPlayingTopic {
-                        if playerModel.isPlaying {
-                            Image(systemName: "speaker.wave.2.fill")
-                                .foregroundColor(.white)
-                                .font(.system(size: 24))
-                        } else  {
-                            Image(systemName: "speaker.fill")
-                                .foregroundColor(.white)
-                                .font(.system(size: 24))
+                        if isPlayingTopic {
+                            if playerModel.isPlaying {
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 24))
+                            } else {
+                                Image(systemName: "speaker.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 24))
+                            }
                         }
                     }
-                }
 
-                VStack(alignment: .leading) {
-                    Text(topic.title)
-                        .lineLimit(2)
-                        .fixedSize(horizontal: false, vertical: true)
+                    VStack(alignment: .leading) {
+                        Text(topic.title)
+                            .lineLimit(2)
+                            .fixedSize(horizontal: false, vertical: true)
 
-                    Text(topic.createdAt.customFormatted())
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                }
-
-                Spacer()
-                
-                if (!isPlayingTopic) {
-                    Menu {
-                        Button(role: .destructive, action: { onDelete() }) {
-                            Label("Remove", systemImage: "trash")
-                        }
-                        Button(action: { userModel.playNext(at: topic.id) }) {
-                            Label("Play Next", systemImage: "text.line.first.and.arrowtriangle.forward")
-                        }
-                        Button(action: { userModel.moveTopicToBack(at: topic.id) }) {
-                            Label("Play Last", systemImage: "text.line.last.and.arrowtriangle.forward")
-                        }
-                    } label: {
-                        Image(systemName: "ellipsis")
-                            .foregroundColor(.black)
-                            .padding()
+                        Text(topic.createdAt.customFormatted())
+                            .font(.caption)
+                            .foregroundColor(.secondary)
                     }
-                    .onTapGesture {
-                        isMenuOpen.toggle()
+
+                    Spacer()
+                    
+                    if (!isPlayingTopic) {
+                        Menu {
+                            Button(role: .destructive, action: { onDelete() }) {
+                                Label("Remove", systemImage: "trash")
+                            }
+                            Button(action: { userModel.playNext(at: topic.id) }) {
+                                Label("Play Next", systemImage: "text.line.first.and.arrowtriangle.forward")
+                            }
+                            Button(action: { userModel.moveTopicToBack(at: topic.id) }) {
+                                Label("Play Last", systemImage: "text.line.last.and.arrowtriangle.forward")
+                            }
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                        .onTapGesture {
+                            isMenuOpen.toggle()
+                        }
                     }
                 }
             }
-        }
-        .padding(.horizontal)
-        .onTapGesture {
-            onPlay()
-        }
+            .padding(.horizontal)
+            .onTapGesture {
+                onPlay()
+            }
 
+            
+        }
         if isMenuOpen {
-            Color.black.opacity(0.001)
-                .edgesIgnoringSafeArea(.all)
-                .onTapGesture {
-                    isMenuOpen = false
-                }
+            Color.black.opacity(0.001) // Transparent overlay
+            .edgesIgnoringSafeArea(.all)
+            .onTapGesture {
+                isMenuOpen = false
+            }
         }
     }
     
