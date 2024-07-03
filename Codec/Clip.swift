98a213ec13cc5957712b28399874e733853ae6c1
Jacob Peddicord <jpmail17@gmail.com>
2024-07-03 16:54:52 -0700

    Begin migration to new backend

---
+++ Codec/Clip.swift
@@ -0,0 +1,8 @@
+//
+//  Clip.swift
+//  Codec
+//
+//  Created by Jacob Peddicord on 7/3/24.
+//
+
+import Foundation
---
+++ Codec/Views/ClipListView.swift
@@ -0,0 +1,8 @@
+//
+//  ClipListView.swift
+//  Codec
+//
+//  Created by Jacob Peddicord on 7/3/24.
+//
+
+import Foundation
