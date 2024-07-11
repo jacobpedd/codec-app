143b5744cdba7e724b4209d75e8be19312391251
Jacob Peddicord <jpmail17@gmail.com>
2024-07-11 14:02:23 -0700

    fix nav bar blocking tap

--- Codec/Views/FeedView.swift
+++ Codec/Views/FeedView.swift
@@ -1,55 +1,57 @@
-//
-//  FeedView.swift
-//  Codec
-//
-//  Created by Jacob Peddicord on 5/22/24.
-//
-
 import SwiftUI
-
 
 struct FeedView: View {
     @EnvironmentObject private var feedModel: FeedModel
-    @State private var fetchingNewTopics: Bool = false
     @State private var showProfile: Bool = false
-
+    
     var body: some View {
         NavigationStack {
-            GeometryReader { geometry in
-                ZStack {
-                    ArtworkScrollFeed()
+            ZStack {
+                // Main content
+                GeometryReader { geometry in
+                    ZStack {
+                        ArtworkScrollFeed()
+                        
+                        VStack {
+                            Spacer()
+                            NowPlayingView()
+                        }
+                    }
+                }
+                .edgesIgnoringSafeArea(.top)
+                
+                VStack {
+                    ZStack {
+                        // Tappable area for previous action
+                        Color.clear
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                feedModel.previous()
+                            }
+                        
+                        // Profile button
+                        HStack {
+                            Spacer()
+                            NavigationLink(destination: ProfileView().environmentObject(feedModel)) {
+                                Image(systemName: "person.crop.circle")
+                                    .imageScale(.large)
+                                    .foregroundColor(.primary)
+                                    .padding()
+                            }
+                            .padding(.trailing, 5)
+                        }
+                    }
+                    .frame(height: 44)
                     
-                    VStack {
-                        Spacer()
-                        NowPlayingView()
-                    }
+                    Spacer()
                 }
             }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: ProfileView().environmentObject(feedModel)) {
-                        Image(systemName: "person.crop.circle")
-                            .imageScale(.large)
-                            .foregroundColor(.primary)
-                    }
-                }
-            }
-        }
-    }
-    
-    private func fetchMoreTopics() {
-        fetchingNewTopics = true
-        Task {
-            await feedModel.loadMoreClips()
-            DispatchQueue.main.async {
-                fetchingNewTopics = false
-            }
+            .navigationBarHidden(true)
         }
     }
 }
 
 #Preview {
-    return ContentView()
+    FeedView()
         .environmentObject(FeedModel())
 }
