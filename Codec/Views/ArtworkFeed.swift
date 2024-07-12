65dc706d435d4acd688cac2751756514fa7d8ef5
Jacob Peddicord <jpmail17@gmail.com>
2024-07-11 19:18:28 -0700

    nicer loading state for art card

---
+++ Codec/Views/ArtworkFeed.swift
@@ -0,0 +1,8 @@
+//
+//  ArtworkFeed.swift
+//  Codec
+//
+//  Created by Jacob Peddicord on 7/11/24.
+//
+
+import Foundation
---
+++ Codec/Views/SwiftUIScrollFeed.swift
@@ -0,0 +1,8 @@
+//
+//  SwiftUIScrollFeed.swift
+//  Codec
+//
+//  Created by Jacob Peddicord on 7/11/24.
+//
+
+import Foundation
